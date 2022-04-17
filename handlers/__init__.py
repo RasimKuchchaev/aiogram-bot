f036d5d7dd6a52fa890a83b445a84103aa45b1a2
RasimKuchchaev <rasimkuchchaev@gmail.com>
2022-04-17 10:08:58 +0300

    Handler echo_m

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,10 @@
-from . import errors
-from . import users
-from . import groups
-from . import channels
+# from . import errors
+# from . import users
+# from . import groups
+# from . import channels
+
+from .errors import dp
+from .users import dp
+
+__all__ = ["dp"]
 
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,3 +1,8 @@
-from . import help
-from . import start
-from . import echo
+# from . import help
+# from . import start
+# from . import echo
+
+# from . import echo_m
+from .echo_m import dp
+
+__all__ = ["dp"]

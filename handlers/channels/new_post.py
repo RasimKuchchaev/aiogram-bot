c35bb50404d0c1257141cc336bbff385b5fb6b6b
RasimKuchchaev <rgkuchchaev@gmail.com>
2023-02-11 22:40:23 +0300

    add edited_channel_post_handler

--- handlers/channels/new_post.py
+++ handlers/channels/new_post.py
@@ -7,3 +7,9 @@ from loader import dp
 async def new_post(message: types.Message):
     logging.info(f"Опубликовано новое сообщение в канале {message.chat.title}.\n"
                  f"{message.text}")
+
+
+@dp.edited_channel_post_handler(content_types=types.ContentType.ANY)
+async def new_post(message: types.Message):
+    logging.info(f"Отредактировано сообщение в канале {message.chat.title}.\n"
+                 f"{message.text}")
